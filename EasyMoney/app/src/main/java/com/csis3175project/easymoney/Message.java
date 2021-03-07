2825c694a4578512b88e7e176e58dbda5dff61e7
Kate <obertase@student.douglascollege.ca>
2021-03-07 14:11:48 -0800

    Created Base App and added DB classes
    Android Studio App for the project
    Moved DB classes to it as well

---
+++ EasyMoney/app/src/main/java/com/csis3175project/easymoney/Message.java
@@ -0,0 +1,10 @@
+package com.csis3175project.easymoney;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
