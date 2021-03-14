7bab933421aaade8ed070f53bee94bbeafa6396c
SohipKim <SoHeeKim@HaeJungui-MacBook-Pro.local>
2021-03-14 16:05:30 -0700

    Sohee
    splash, welcome, login activities
    
    color, button, text styles xml

---
+++ EasyMoney/app/src/main/java/com/csis3175project/easymoney/Welcome.java
@@ -0,0 +1,16 @@
+package com.csis3175project.easymoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
