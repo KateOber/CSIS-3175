017bdd0e17658eccea5bec8f55c5d32bcb6d45b4
SohipKim <sohee0819@gmail.com>
2021-03-27 19:01:35 -0700

    Adding errors to text fields

--- EasyMoney/app/src/main/java/com/csis3175project/easymoney/LoginActivity.java
+++ EasyMoney/app/src/main/java/com/csis3175project/easymoney/LoginActivity.java
@@ -2,21 +2,21 @@ package com.csis3175project.easymoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 
 public class LoginActivity extends AppCompatActivity {
     EMDatabase myhelper;
     private String inputUserName;
-    private String inputPassword;
+    private String inputUserPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,35 +24,49 @@ public class LoginActivity extends AppCompatActivity {
 
         myhelper = new EMDatabase(this);
 
-//        EditText testUserName = findViewById(R.id.userName);
-//        TextInputEditText testPassword = findViewById(R.id.userPassword);
-
-        TextInputLayout testUserName = findViewById(R.id.userName);
-        TextInputLayout testPassword = findViewById(R.id.userPassword);
-
-        TextInputEditText
+        TextInputEditText password = findViewById(R.id.inputPassword);
+        TextInputEditText userName = findViewById(R.id.inputUserName);
 
         ImageView btnBack = findViewById(R.id.btnBack);
         Button btnLogin = findViewById(R.id.btnSignUp);
 
         TextView btnSignUp = findViewById(R.id.btnGoSignUpWelcome);
 
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                inputUserName = testUserName.getText().toString();
-//                inputPassword = testPassword.getText().toString();
-//                boolean ischecked =
-//                        myhelper.checkLogin(inputUserName, inputPassword);
-//
-//                if(ischecked) {
-////                    startActivity(new Intent(LoginActivity,this. --put the name of screen--.class)
-//                    Toast.makeText(LoginActivity.this, "Valid user", Toast.LENGTH_LONG).show();
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "Invalid user", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                inputUserName = userName.getText().toString();
+                inputUserPassword = password.getText().toString();
+
+                System.out.println(inputUserName + " : " + inputUserPassword);
+
+
+                boolean ischecked =
+                        myhelper.checkLogin(inputUserName, inputUserPassword);
+
+                if(ischecked) {
+                    Toast.makeText(LoginActivity.this, "Welcome," + inputUserName, Toast.LENGTH_LONG).show();
+//                    startActivity(new Intent(LoginActivity,this. --put the name of screen--.class)
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please check username or password again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- EasyMoney/app/src/main/java/com/csis3175project/easymoney/RegisterActivity.java
+++ EasyMoney/app/src/main/java/com/csis3175project/easymoney/RegisterActivity.java
@@ -2,20 +2,22 @@ package com.csis3175project.easymoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.textfield.TextInputEditText;
+import java.io.*;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     EMDatabase myhelper;
 
     private String inputUserName;
     private String inputUserEmail;
-    private String inputUsePassword;
+    private String inputUserPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +26,107 @@ public class RegisterActivity extends AppCompatActivity {
 
         myhelper = new EMDatabase(this);
 
-        TextInputEditText userName = findViewById(R.id.userName);
-        TextInputEditText userEmail = findViewById(R.id.userEmail);
-        TextInputEditText userPassword = findViewById(R.id.userPassword);
+        TextInputEditText userName = findViewById(R.id.inputName);
+        TextInputEditText userEmail = findViewById(R.id.inputEmail);
+        TextInputEditText userPassword = findViewById(R.id.inputPassword);
 
         Button btnSignUp = findViewById(R.id.btnSignUp);
         TextView btnGoSignIn = findViewById(R.id.btnGoSignUpWelcome);
+        ImageView btnBack = findViewById(R.id.btnBack);
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                inputUserName = userName.getText().toString();
+//                inputUserEmail = userEmail.getText().toString();
+//                inputUserPassword = userPassword.getText().toString();
+//
+//                if(!(inputUserName ==null && inputUserEmail==null && inputUserPassword ==null)) {
+//                    myhelper.insertUSERData(inputUserName, inputUserEmail, inputUserPassword, 0);
+//                    System.out.println("s"); //test
+//                    Toast.makeText(RegisterActivity.this,
+//                            "Has been successfully registered", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+//                }
+//
+//                if(inputUserName.length() ==0){
+//                    Toast.makeText(RegisterActivity.this, "Please Enter user name", Toast.LENGTH_SHORT).show();
+//                    userEmail.requestFocus();
+//                    return;
+//                }
+//                if(inputUserEmail.length() ==0){
+//                    Toast.makeText(RegisterActivity.this, "Please Enter your Email", Toast.LENGTH_SHORT).show();
+//                }
+//                if(inputUserPassword.length()==0){
+//                    Toast.makeText(RegisterActivity.this, "Please Enter password", Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        });
+//        btnGoSignIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+//            }
+//        });
+//        btnBack.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onBackPressed();
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //btn Sign up
+        switch (v.getId()) {
+            case R.id.btnSignUp: {
+
+                TextInputEditText userName = findViewById(R.id.inputName);
+                TextInputEditText userEmail = findViewById(R.id.inputEmail);
+                TextInputEditText userPassword = findViewById(R.id.inputPassword);
 
                 inputUserName = userName.getText().toString();
                 inputUserEmail = userEmail.getText().toString();
-                inputUsePassword = userPassword.getText().toString();
+                inputUserPassword = userPassword.getText().toString();
 
-                if(inputUserName ==null && inputUserEmail==null && inputUsePassword ==null) {
-                    myhelper.insertUSERData(inputUserName, inputUserEmail, inputUsePassword, 0);
-                    Toast.makeText(RegisterActivity.this, "Register succeeded", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Toast.makeText(RegisterActivity.this, "Register failed", Toast.LENGTH_LONG).show();
+                if (!(inputUserName == null && inputUserEmail == null && inputUserPassword == null)) {
+                    myhelper.insertUSERData(inputUserName, inputUserEmail, inputUserPassword, 0);
+                    System.out.println("s"); //test
+                    Toast.makeText(RegisterActivity.this,
+                            "Has been successfully registered", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 }
 
+                if (inputUserName.length() == 0) {
+                    Toast.makeText(RegisterActivity.this, "Please Enter user name", Toast.LENGTH_SHORT).show();
+                    userName.requestFocus();
+                    return;
+                }
+                if (inputUserEmail.length() == 0) {
+                    Toast.makeText(RegisterActivity.this,
+                            "Please Enter your Email", Toast.LENGTH_SHORT).show();
+                    userEmail.requestFocus();
+                }
+                if (inputUserPassword.length() == 0) {
+                    Toast.makeText(RegisterActivity.this,
+                            "Please Enter password", Toast.LENGTH_SHORT).show();
+                    userPassword.requestFocus();
+                }
+                break;
             }
-        });
 
+            case R.id.btnGoSignIn:{
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                break;
+            }
+            case R.id.btnBack:{
+                onBackPressed();
+            }
+
+        }
     }
 }
