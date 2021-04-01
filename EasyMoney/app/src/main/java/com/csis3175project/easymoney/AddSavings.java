f8628d7c1de09b34e22b66b5d6bdea2468d81197
SohipKim <SoHeeKim@HaeJungui-MBP.hitronhub.home>
2021-03-31 21:18:48 -0700

    fixing conflicts on add savings

--- EasyMoney/app/src/main/java/com/csis3175project/easymoney/AddSavings.java
+++ EasyMoney/app/src/main/java/com/csis3175project/easymoney/AddSavings.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -29,7 +30,7 @@ public class AddSavings extends AppCompatActivity {
         databaseHelper = new EMDatabase(this);
 
         Button addSavingsbtn = findViewById(R.id.addSavingsbtn);
-        Button addSavingsBackbtn = findViewById(R.id.addSavingsBackbtn);
+        ImageView addSavingsBackbtn = findViewById(R.id.btnBack);
         TextInputEditText savingsAmount_txt = findViewById(R.id.SavingsammountInput);
 
         addSavingsbtn.setOnClickListener(new View.OnClickListener() {
@@ -39,28 +40,29 @@ public class AddSavings extends AppCompatActivity {
                 if(savingsAmount_txt.getText().toString().isEmpty())
                     savingsAmount_txt.setBackgroundColor(Color.parseColor("#20D81B60"));
                 else
-                {
-                    savingsAmount_txt.setBackgroundColor(Color.parseColor("#E0E0E0"));
-                    double amount = Double.parseDouble(savingsAmount_txt.getText().toString());
-                    double currentSavings = databaseHelper.getSavings(username);
-                    boolean savingsUpdated = databaseHelper.updateSavings(currentSavings+amount, username);
+                    {
+                        savingsAmount_txt.setBackgroundColor(Color.parseColor("#E0E0E0"));
+                        double amount = Double.parseDouble(savingsAmount_txt.getText().toString());
+                        double currentSavings = databaseHelper.getSavings(username);
+                        boolean savingsUpdated = databaseHelper.updateSavings(currentSavings+amount, username);
 
-                    if(savingsUpdated)
+                        if(savingsUpdated)
                         Toast.makeText(AddSavings.this,
                                 "Saving Added", Toast.LENGTH_SHORT).show();
 
-                    else
-                        Toast.makeText(AddSavings.this,
-                                "Error", Toast.LENGTH_SHORT).show();
-                }
+                        else
+                            Toast.makeText(AddSavings.this,
+                            "Error", Toast.LENGTH_SHORT).show();
+                    }
             }
         });
 
         addSavingsBackbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(AddSavings.this, ExpenseTracker.class));
+                onBackPressed();
             }
         });
     }
 }
+
